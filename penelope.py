34bb0cdcfb9558b3eeaf7ae3c124091593bbff1c
z-adams <chryxus@gmail.com>
2019-06-26 16:56:42 -0700

    Added improved options for plot_dat.py, penelope.py now working

--- penelope.py
+++ penelope.py
@@ -3,60 +3,47 @@
 
 from penelopetools.api.input.penelope.description import Description
 from penelopetools.api.input.penelope.particle import ELECTRON
-from penelopetools.api.program.shower import Shower2010
+from penelopetools.api.runner.shower import Shower2010
 from penelopetools.api.input.penelope.materials import *
+from penelopetools.api.input.pengeom.usergeometry.substrate import Substrate
 from penelopetools.api.input.shower2010.io import InputFile
+from penelopetools.api.input.shower2010.io import XMLFile
 from penelopetools.api.input.shower2010.source import Source
 from penelopetools.api.input.shower2010.jobproperties import Simulation
 from penelopetools.api.input.shower2010.options import Options
 
+BEAM_ENERGY=350e3 # 
 
 ### Microscope ("source")
-src = Source(particle=ELECTRON, beam_energy=350e3) # rest of params are defaults
+src = Source(particle=ELECTRON, beam_energy=BEAM_ENERGY)
 
-### Geometry
-# Select geometry (substrate)
+### Materials
 
 # Create and select material
 Cd = Element('Cd')
 Te = Element('Te')
 
 elems = Elements()
-# elems.add(symbol, fraction=1.0)
-# elems.add_element(element)
 elems.add('Cd', fraction=0.5)
 elems.add('Te', fraction=0.5)
 elems.userdensity = 5.85
-# penelopetools.api.input.penelope.materials.SimulationParamters
 
-# Parameters:
-#sim = SimulationParameters()
-# absorption of e-, e+, phot
-#sim.absorption_energy_electron(50)
-#sim.absorption_energy_photon(50)
-#sim.absorption_energy_positron(50)
-
-# elastic scattering parameters C1, C2
-#sim.elastic_scattering_paramters_c1(0.1)
-#sim.elastic_scattering_paramters_c2(0.1)
-
-# cutoff energy loss
-#sim.cutoff_energyloss_inelasticcollisions(50)
-#sim.cutoff_energyloss_bremsstrahlungemission(50)
-
-# can get xml/in config files
-#sim.to_inputfile()
-#sim.to_xml()
-
-params = SimulationParameters()  # defaults (e.g. e- energy=50), not "use default"
+# Parameters: (equivalent to "use default simulation parameters" checkbox
+energy_parameters = {
+        'absorption_energy_electron': BEAM_ENERGY * 0.1,
+        'absorption_energy_photon': BEAM_ENERGY * 0.01,
+        'absorption_energy_positron': BEAM_ENERGY * 0.1,
+        'elastic_scattering_parameter_c1': 0.2,
+        'elastic_scattering_parameter_c2': 0.2,
+        'cutoff_energyloss_inelasticcollisions': BEAM_ENERGY * 0.1,
+        'cutoff_energyloss_bremsstrahlungemission': BEAM_ENERGY * 0.01 
+        }
+params = SimulationParameters(**energy_parameters)
 
 mat = Material(name="CdTe", filename="CdTe.mat", elements=elems, simulation_parameters=params)
 
 mats = Materials()
-mats.add(mat, userid=None)
-
-### Geometry
-#geom = Geometry(title='example')
+mats.add(mat, userid=1)
 
 ### Simulation
 sim = Simulation(randomnumberseeds=None, secondary_particles=True, trajectories=10)
@@ -67,13 +54,15 @@ opt = Options()
 opt.description = Description(title='example')
 opt.source = src
 opt.materials = mats
-#opt.geometry = defaults to substrate
 opt.simulation = sim
+opt.geometry = Substrate(substrate_material_id=1)
 
-io = InputFile()
+#io = InputFile()
+x = XMLFile()
 
-with open('test.in', 'w') as f:
-    io.write(f, opt)
+with open('test.xml', 'w') as f:
+    x.write(f, opt)
 
-#sim = Shower2010()
-#runner = program.get_runner()
+CONFIG = "/home/zander/.pypenelope/pypenelope.cfg"
+shwr = Shower2010(CONFIG, options=opt)
+shwr.start()
--- plot_dat.py
+++ plot_dat.py
@@ -1,8 +1,12 @@
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 import os
 from parser import *
+
+DATA_FRAC = 10 # graph every 10th data point
+LINE_ONLY = True
 
 print "current location: {}".format(os.getcwd())
 filename = raw_input("Where is the .dat file?: ")
@@ -13,20 +17,21 @@ first = True
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-#tracks = []
+tracks = []
 
 for traj in trajectories:
-    #tracks.append([[], [], []])
-    for event in traj.events:
+    tracks.append([[], [], []])
+    for index, event in enumerate(traj.events):
         color = 'r' if first else 'b'
+        if (index % 10 == 0 and not LINE_ONLY) or (first and LINE_ONLY):
+            ax.scatter(event['x'], event['y'], event['z'], c=color, marker='o')
+        tracks[-1][0].append(event['x'])
+        tracks[-1][1].append(event['y'])
+        tracks[-1][2].append(event['z'])
         first = False
-        ax.scatter(event['x'], event['y'], event['z'], c=color, marker='o')
-        #tracks[-1][0].append(event['x'])
-        #tracks[-1][1].append(event['y'])
-        #tracks[-1][2].append(event['z'])
         
-#for track in tracks:
-#    ax.plot(track[0], track[1], track[2], zdir=track[2], c=color, alpha=0.4)
+for track in tracks:
+    ax.plot(track[0], track[1], track[2], zdir=track[2], c=color, alpha=0.4)
 
 ax.set_xlabel("x")
 ax.set_ylabel("y")
